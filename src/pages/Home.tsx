1f45cc8b95026dcbae8320a5642a06a0615d3936
Alberto <albertoig8@hotmail.com>
2021-06-22 13:37:30 -0300

    Preparando ambiente para estilização

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import illustrationImg from '..assets/images/illustration.svg'
+import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
