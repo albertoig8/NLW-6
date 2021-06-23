2f9a258e154d121bfae776fc247d1043b66f4460
Alberto <albertoig8@hotmail.com>
2021-06-23 19:51:27 -0300

    Criando fluxo de criação de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,17 +1,18 @@
 import { FormEvent, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
-// import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
-    // const { user } = useAuth();
-
+    const { user } = useAuth();
+    const history = useHistory();
     const [newRoom, setNewRoom] = useState('');
 
     async function handleCreateRoom(event: FormEvent) {
@@ -20,6 +21,16 @@ export function NewRoom() {
         if (newRoom.trim() === '') {
             return;
         }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
     }
 
     return (
